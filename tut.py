e9e75e27f27c1832f8304e63d6e8b0910266a72f
Alejandro Giacometti <alejandro.giacometti@gmail.com>
2016-10-27 16:30:31 +0100

    setup backend for redux frontend and webpack

--- tut.py
+++ tut.py
@@ -2,6 +2,7 @@
 import os
 
 from flask import Flask, g, jsonify, render_template, flash, redirect, url_for, request
+from flask_webpack import Webpack
 from tinydb import TinyDB, Query
 
 app = Flask(__name__)
@@ -10,15 +11,21 @@ app.config.from_object(__name__)
 app.config.update(dict(
     DB_PATH=os.path.join(app.root_path, 'db/db.json'),
     SECRET_KEY='horse stapling or something like that',
+    WEBPACK_MANIFEST_PATH=os.path.join(
+        app.root_path, 'static', 'dist', 'manifest.json'),
 ))
 
 app.config.from_envvar('TUT_SETTINGS', silent=True)
+
+webpack = Webpack()
+webpack.init_app(app)
 
 
 def connect_db():
     # connects to database
     db = TinyDB(app.config['DB_PATH'])
     return db
+
 
 def get_db():
     """Opens a db connection if there is none for the current application
@@ -27,6 +34,7 @@ def get_db():
     if not hasattr(g, 'tinydb'):
         g.tinydb = connect_db()
     return g.tinydb
+
 
 @app.teardown_appcontext
 def close_db(error):
@@ -48,7 +56,8 @@ def show_comments():
 
     return jsonify(comments)
 
-@app.route('/api/comments', methods=['POST'])
+
+@app.route('/api/comment', methods=['POST'])
 def insert_comment():
     db = get_db()
     Comment = Query()
@@ -62,7 +71,7 @@ def insert_comment():
     db.insert(new_comment)
     flash('Comment {} inserted'.format(next_id))
 
-    return redirect(url_for('show_comments'))
+    return jsonify(new_comment)
 
 
 if __name__ == "__main__":
